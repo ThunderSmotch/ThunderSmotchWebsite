9f7a05f257f853fe9689892868b67a22e79e11ff
thundersmotch <2165764+ThunderSmotch@users.noreply.github.com>
2020-07-17 11:16:13 +0100

    Added Google Analytics

--- src/templates.js
+++ src/templates.js
@@ -19,6 +19,15 @@ let headHTML = `
 <link rel="stylesheet" type="text/css" href="/style/webtex.css">
 <link rel="stylesheet" type="text/css" href="/style/style.css">
 <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-168636305-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-168636305-2');
+</script>
 `;
 
 //Builds any page by inserting the head and the content
