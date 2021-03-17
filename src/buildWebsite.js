7c2873ef0b0d65cb9f0a3a259ff2059adf67c73d
thundersmotch <2165764+ThunderSmotch@users.noreply.github.com>
2021-03-17 18:48:26 +0000

    Added initial chapter on generic concepts in CM and QI

--- src/buildWebsite.js
+++ src/buildWebsite.js
@@ -23,6 +23,10 @@ const sitemap = require("./sitemapBuilder");
 
 //MAYBE Refactor code so that instead of data.json we use yaml metadata at the top of each file...
 
+//TODO Footnotes!
+
+//TODO Todo Notes compatibility! HACKish at first
+
 //////////////////////Building the Website//////////////////////////////
 ensureDirectoryExists(config.dev.outdir);
 moveFilesFrom('js');
