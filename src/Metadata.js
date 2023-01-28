29cbf99e767027054e04874d4888385cf1b2c3b4
Carlos Couto <2165764+ThunderSmotch@users.noreply.github.com>
2023-01-28 10:55:18 +0000

    Style changes

--- src/Metadata.js
+++ src/Metadata.js
@@ -2,17 +2,16 @@ module.exports = {GetMetadata, GetDefaultField};
 const fm = require("front-matter");
 const fs = require("fs");
 
-
 const config = require("./config");
 const Utils = require("./Utils");
 
-// TODO Most pages need some sensible defaults
-// I should fix the GetMetadata function so that the attributes returned from frontmatter are replacing
-// the default ones
 let default_metadata = 
 {
+// Required fields
 title: "ThunderSmotch",
 description: "ThunderSmotch - Maths/Physics/Programming",
+
+// Optional fields
 sidebar: false, // Should a sidebar be generated at this point
 hidden: false,
 navbar: true, // If navbar should be drawn
