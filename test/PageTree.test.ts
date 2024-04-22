c052b1046ae660bb94af70f126bf5cfcbb9dabd1
Carlos Couto <2165764+ThunderSmotch@users.noreply.github.com>
2024-04-22 15:58:12 +0100

    test: PageTree initial tests

--- test/PageTree.test.ts
+++ test/PageTree.test.ts
@@ -1,3 +1,32 @@
-import {} from '../src/PageTree';
+import { GetPageTree } from '../src/PageTree';
 
-// TODO PageTree tests
+import { GetSubDirs } from '../src/FileUtils';
+
+jest.mock('../src/FileUtils');
+
+describe("GetPageTree", () => {
+    it("returns a one level PageTree if there are no subdirectories", () => {
+        jest.mocked(GetSubDirs).mockReturnValue([]);
+
+        expect(GetPageTree().pages).toStrictEqual({});
+    });
+
+    it("returns a two level deep PageTree correctly", () => {
+        jest.mocked(GetSubDirs).mockReturnValueOnce(["A", "B", "C"]).mockReturnValue([]);
+
+        let pageTree = GetPageTree();
+
+        expect(Object.keys(pageTree.pages)).toHaveLength(3);
+        expect(Object.values(pageTree.pages)[0].pages).toStrictEqual({});
+    });
+
+    it("returns a three level deep PageTree correctly", () => {
+        jest.mocked(GetSubDirs).mockReturnValueOnce(["A"]).mockReturnValueOnce(["B"]).mockReturnValue([]);
+
+        let pageTree = GetPageTree();
+
+        expect(Object.keys(pageTree.pages)).toStrictEqual(["A"]);
+        expect(Object.keys(pageTree.pages["A"].pages)).toStrictEqual(["B"]);
+        expect(pageTree.pages["A"].pages["B"].pages).toStrictEqual({});
+    });
+});
